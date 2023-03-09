68edb1a9b058cdb8a0fd97eff13e641be179e583
Ivan Stich <huvana99@gmail.com>
2023-03-09 09:23:34 +0300

    all save what i can

---
+++ gulp/gulp-start/gulpfile.js
@@ -0,0 +1,12 @@
+const { src, dest } = require('gulp');
+
+
+const scss = require('gulp-sass');
+
+
+function styles(){
+    return src('app/scss/style.scss')
+        .pipe(scss())
+        .pipe(dest('app/css'))
+}
+exports.styles = styles;
---
+++ mypracticl/lamborghini/js/main.js
@@ -0,0 +1,21 @@
+// const swiper = new Swiper('.swiper', {
+//     // Optional parameters
+//     // direction: 'horizontal',
+//     loop: true,
+
+//     // Navigation arrows
+//     navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//     },
+// });
+// let map;
+
+// function initMap() {
+//   map = new google.maps.Map(document.getElementById("map"), {
+//     center: { lat: -34.397, lng: 150.644 },
+//     zoom: 8,
+//   });
+// }
+
+// window.initMap = initMap;
